6b26e608013c1f25af7edeee656456a091309fa2
mfaldutof <manuel.falduto@gmail.com>
2026-06-18 14:30:12 -0500

    Change static file serving directory in server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function writeQuotes(quotes) {
 }
 
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.get('/health', (req, res) => res.status(200).send('ok'));
 
